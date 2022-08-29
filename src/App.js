3a535b3ea78c10ca68984c4433be00afcb6c5d47
Guy dahan <guydaane@gmail.com>
2022-08-29 16:25:29 +0300

    finalized app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
-import socketIO from 'socket.io-client';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Home } from "./components/Home";
-import { ChatPage } from "./components/ChatPage";
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+import Home from "./components/Home"
+import ChatPage from "./components/ChatPage";
+import socketIO from "socket.io-client"
 
-const socket = socketIO.connect('http://localhost:4000');
+const socket = socketIO.connect("http://localhost:4000")
 function App() {
   return (
-    <div>
-    <p>Welcome to CHUTUP!</p>
-  </div>
+    <BrowserRouter>
+        <div>
+          <Routes>
+            <Route path="/" element={<Home socket={socket}/>}></Route>
+            <Route path="/chat" element={<ChatPage socket={socket}/>}></Route>
+          </Routes>
+    </div>
+    </BrowserRouter>
+    
   );
 }
 
